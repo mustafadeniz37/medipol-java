6720b66cea8f06e7f91b0f35f187abb53f32c4a8
mustafadeniz37 <60935981+mustafadeniz37@users.noreply.github.com>
2020-03-03 15:37:46 +0300

    Update karzarar.java

--- karzarar.java
+++ karzarar.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Company
 {
     public static void main(String[] args)
-	{
+    {
         System.out.println("Kar miktarını giriniz.");
         Scanner kar = new Scanner(System.in);
         int karMiktari = kar.nextInt();
