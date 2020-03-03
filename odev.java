887930f02a2b388049129cf73512894dbd1bc53c
mustafadeniz37 <60935981+mustafadeniz37@users.noreply.github.com>
2020-03-03 17:44:24 +0300

    Update odev.java

--- odev.java
+++ odev.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main
+public class odev
 {
     public static void main(String[] args)
     {
@@ -10,7 +10,7 @@ public class Main
         System.out.println("Bir sayı girin:");
         int degisken = kaydet.nextInt();
         System.out.println("Girdi: " + degisken);
-		
+
         if(degisken == 0)
         {
             System.out.print("0 değerinin tüm pozitif kuvvetleri sıfırdır.");
@@ -19,22 +19,22 @@ public class Main
         {
             System.out.print("-1'in tüm pozitif tek kuvvetleri -1'e, diğer tüm pozitif çift kuvvetleri ise 1'e eşittir.");
         }
-        else 
+        else
         {
             for(int i=1; i<=degisken; i++)
             {
-		System.out.print("n^1");
-		double hesaplama = Math.pow(i, 1);
-		System.out.print(hesaplama);
-		System.out.print("n^2");
-		hesaplama = Math.pow(i, 2);
-		System.out.print(hesaplama);
-		System.out.print("n^3");
-		hesaplama = Math.pow(i, 3);
-		System.out.print(hesaplama);
-		System.out.print("n^4");
-		hesaplama = Math.pow(i, 4);
-		System.out.println(hesaplama);
+                System.out.print("n^1");
+                double hesaplama = Math.pow(i, 1);
+                System.out.print(hesaplama);
+                System.out.print("n^2");
+                hesaplama = Math.pow(i, 2);
+                System.out.print(hesaplama);
+                System.out.print("n^3");
+                hesaplama = Math.pow(i, 3);
+                System.out.print(hesaplama);
+                System.out.print("n^4");
+                hesaplama = Math.pow(i, 4);
+                System.out.println(hesaplama);
             }
         }
     }
